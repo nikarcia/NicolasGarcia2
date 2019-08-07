2278ebea3611a74218e6cad6fcbd7e2da2cc163d
NicolasGarcia2 <38087848+nikarcia@users.noreply.github.com>
2019-08-07 08:20:27 -0500

    Create newtons.py

---
+++ metodos/newtons.py
@@ -0,0 +1,12 @@
+import math
+def newtons(f, df, xi, e):
+	cont=0
+	while(cont<e):
+		x=xi-f(xi)/df(xi)
+		xi=x
+		cont+=1
+		print('x: ',x)
+
+# f1(e^x)=e^x
+newtons(lambda x: pow(x-1.8974,3), lambda x: pow(3*(x-1.8974),2), 0, 9)
+© 2019 GitHub, Inc.
